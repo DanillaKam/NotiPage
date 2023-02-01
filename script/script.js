99bf4c51de580cba4ff2f5bdb5d1c3010d71f9d6
DanillaKam <122106121+DanillaKam@users.noreply.github.com>
2023-02-01 11:33:16 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,41 +1,37 @@
-notifications = document.querySelectorAll('.nottifications')
-notificationsUnread = document.querySelectorAll('.unread')
-setAllReadButton = document.getElementById('set-all-btn')
-redDot = document.querySelectorAll('#red-dot')
-notificationsNumber = document.getElementById('notifications-number')
+const notifications = document.querySelectorAll('.nottifications')
+let notificationsUnread = document.querySelectorAll('.unread')
+const setAllReadButton = document.getElementById('set-all-btn')
+const redDot = document.querySelectorAll('#red-dot')
+const notificationsNumber = document.getElementById('notifications-number')
 
-notificationsNumber.innerHTML = notificationsUnread.length
-
-//dodanie kropek do klasy unread
-for (var i = 0; i < notificationsUnread.length; i++) {
-	notificationsUnread[i].querySelector('.dot').innerHTML = '<i id="red-dot" class="fa-solid fa-circle"></i>'
-	redDot = document.querySelectorAll('#red-dot')
-	console.log(redDot)
-}
-// wyczyszcenie nieprzeczytanych
-function setAllRead() {
-	for (var i = 0; i < notifications.length; i++) {
-		notifications[i].classList.remove('unread')
-	}
-	for (var i = 0; i < redDot.length; i++) {
-		redDot[i].style.display = 'none'
-	}
-
-	notificationsUnread = document.querySelectorAll('.unread')
+const showActualNotiNumber = () => {
+	let notificationsUnread = document.querySelectorAll('.unread')
 	notificationsNumber.innerHTML = notificationsUnread.length
 }
 
-//czyszcenie samodzielnie
-for (var i = 0; i < notificationsUnread.length; i++) {
-	setRead(i)
-	function setRead(i) {
-		notificationsUnread[i].addEventListener('click', e => {
-			notificationsUnread[i].querySelector('.dot').style.display = 'none'
-			notificationsUnread[i].classList.remove('unread')
-			notificationsUnreadRest = document.querySelectorAll('.unread')
-			notificationsNumber.innerHTML = notificationsUnreadRest.length
-		})
-	}
+const addRedDot = () => {
+	notificationsUnread.forEach(noti => {
+		noti.querySelector('.dot').innerHTML = '<i id="red-dot" class="fa-solid fa-circle"></i>'
+	})
 }
-
+const setAllRead = () => {
+	notificationsUnread = document.querySelectorAll('.unread')
+	notificationsUnread.forEach(noti => {
+		noti.classList.remove('unread')
+		noti.querySelector('.dot').innerHTML = ''
+	})
+	notificationsNumber.innerHTML = 0
+}
+const setUnread = () => {
+	notificationsUnread.forEach(noti => {
+		noti.addEventListener('click', () => {
+			noti.classList.remove('unread')
+			showActualNotiNumber()
+			noti.querySelector('.dot').innerHTML = ''
+		})
+	})
+}
+showActualNotiNumber()
+addRedDot()
+setUnread()
 setAllReadButton.addEventListener('click', setAllRead)
